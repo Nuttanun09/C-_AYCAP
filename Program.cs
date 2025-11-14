e4cd705c3b4bf6f7466a42f8ac17435feb9c2edd
Nuttanun Kanjanapirom <nuttanun.kanjanapirom@krungsri.com>
2025-11-14 10:46:01 +0700

    C#AYCAP

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 0; i < 5; i++)
+{
+    Guid id4 = Guid.CreateVersion7(); // .NewGuid(); // UUID v4
+    string secret = id4.ToString();
+    Console.WriteLine($"Secret: {secret}");
+}
